671bbe43e1548dd240436170af8372d101d6ffd8
Eduardo Michetti <94409995+EduardoMichetti@users.noreply.github.com>
2024-03-02 18:26:18 -0300

    Add project files.

---
+++ Exercise02/Program.cs
@@ -0,0 +1,55 @@
+﻿using Exercise02.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Entre com o número de produtos: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Dados do produto #" + i + ":");
+                Console.Write("Comum, usado ou importado (c/u/i)? ");
+                char type = char.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                String name = Console.ReadLine();
+                Console.Write("Preço: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (type == 'c')
+                {
+                    list.Add(new Product(name, price));
+                }
+                else if (type == 'u')
+                {
+                    Console.Write("Fabricado em (DD/MM/YYYY)? ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, date));
+                }
+                else
+                {
+                    Console.Write("Taxa alfandegária (0.00): ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportedProduct(name, price, customsFee));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ETIQUETAS DE PREÇO:");
+            foreach (Product prod in list)
+            {
+                Console.WriteLine(prod.PriceTag());
+            }
+        }
+    }
+}
